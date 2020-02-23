d9c2da8b3fb3109824332e60a1386db5d0de0c39
Achref Bouadila <a_bouadila@live.fr>
2020-02-23 16:57:54 +0100

    first commit

---
+++ src/main/java/com/ashref/soap/ws/CountryDetailsSoapWsApplication.java
@@ -0,0 +1,13 @@
+package com.ashref.soap.ws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountryDetailsSoapWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CountryDetailsSoapWsApplication.class, args);
+	}
+
+}
